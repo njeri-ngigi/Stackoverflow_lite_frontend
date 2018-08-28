508e0f4178f527b0b736e2011664a3221edc9581
njeri-ngigi <ngigi.shalon@students.jkuat.ac.ke>
2018-08-28 12:23:06 +0300

    [FEATURE] Add edit profile feature

---
+++ js/profile.js
@@ -0,0 +1,24 @@
+function showPostQuestion(divId) {
+    if (divId == 'postQuestion') {
+        document.getElementById('postQuestion').style.display = 'block';
+        document.getElementById('profileDetails').style.display = 'none';
+  }
+    if (divId == 'profileDetails') {
+        document.getElementById('postQuestion').style.display = 'none';
+        document.getElementById('profileDetails').style.display = 'block';
+  }  
+}
+function showEditProfile(divId) {
+    if (divId == 'editProfile') {
+        document.getElementById('editProfile').style.display = 'block';
+        document.getElementById('main').style.display = 'none';
+    }
+    if (divId == 'main') {
+        document.getElementById('editProfile').style.display = "none";
+        document.getElementById('main').style.display = "block";
+    }
+}
+function showEditPassword(){
+    document.getElementById('changePassword').style.display = 'block';
+    document.getElementById("changePasswordButton").style.display = "none";
+}
